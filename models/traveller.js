c93f9b72ccdd8d558027cc0151d6e917fa3618c1
CodeClan Student <codeclanstudent@macbook-pro-2.home>
2020-10-07 15:37:34 +0100

    Commit

---
+++ models/traveller.js
@@ -0,0 +1,48 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const startLocations = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return startLocations;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const endLocations = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  return endLocations;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  const result = this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+  return result;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const result = this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+  return result;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  const total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  }, 0);
+  return total;
+};
+
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   const result = this.journeys.map((journey) => {
+//     return journey.transport;
+//   })
+//   return result;
+// };
+
+
+module.exports = Traveller;
